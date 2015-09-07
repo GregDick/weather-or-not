22531b8d428472e1dc54c30f1f72b943ea1122f8
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-09-07 13:37:20 -0500

    added start script and Readme

---
+++ README.md
@@ -0,0 +1,6 @@
+#weather-or-not
+Learning about Angular and HTML5's geolocation to get the current weather.
+Install dependencies :
+npm install;
+Serve locally :
+npm start;
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,27 @@
 angular
-  .module('weatherOrNot', [])
+  .module('weatherOrNot', ['ngRoute'])
+
+  .config(function($routeProvider){
+    $routeProvider
+      .when('/:zipCode', {
+        templateUrl: 'views/zip.html'
+      })
+  })
+
 
   .controller('Main', function($http){
     var vm = this;
 
-    $http
-      .get('http://api.wunderground.com/api/1a83bcf806654e1a/geolookup/conditions/q/autoip.json')
-      .success(function(data){
-        vm.temperature = data.current_observation.temp_f;
-        vm.place = data.current_observation.display_location.full;
-        vm.humidity = data.current_observation.relative_humidity;
-      })
+    navigator.geolocation.getCurrentPosition(function (geoposition) {
+      var lat = geoposition.coords.latitude;
+      var long = geoposition.coords.longitude;
+
+      $http
+        .get(`http://api.wunderground.com/api/1a83bcf806654e1a/conditions/q/${lat},${long}.json`)
+        .success(function(data){
+          vm.temperature = data.current_observation.temp_f;
+          vm.place = data.current_observation.display_location.full;
+          vm.humidity = data.current_observation.relative_humidity;
+        })
+    })
   })
